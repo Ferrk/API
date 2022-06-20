d9c15ca005ac80fcdf887b15d4b7199ab54f514f
Ferrk <qeri4gyw@mail.ru>
2022-06-20 21:55:27 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+                post.phone = "+7 (999)-999-99-99";
+                post.surname = "Иванов";
+post.subscription = String.valueOf(true);
+        Post.birthday = new FormDate();
+        Post.birthday.day = 13;
+        Post.birthday.month = 5;
+        Post.birthday.year = 1997;
+    }
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    public static FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+
+
+
+
+}
